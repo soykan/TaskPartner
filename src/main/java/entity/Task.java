7b66065b95670e2b6c2b1a5b22abec58f2d6a240
soykan <soykanerturk@gmail.com>
2022-07-04 00:05:41 +0300

    added boilerplate code for register & login functionalites

---
+++ src/main/java/entity/Task.java
@@ -0,0 +1,18 @@
+package entity;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Task {
+	private Long id;
+	private String name;
+	/*
+	 * Add 
+	 * 		duration, 
+	 * 		start time, 
+	 * 		end time,
+	 * 		user (foreign key)
+	 * Here
+	 */
+}

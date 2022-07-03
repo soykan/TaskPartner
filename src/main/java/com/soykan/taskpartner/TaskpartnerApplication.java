0a29020919d51aa3e87dc640f5b960df74e61f3f
soykan <soykanerturk@gmail.com>
2022-07-03 18:40:58 +0300

    fresh project

---
+++ src/main/java/com/soykan/taskpartner/TaskpartnerApplication.java
@@ -0,0 +1,13 @@
+package com.soykan.taskpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskpartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskpartnerApplication.class, args);
+	}
+
+}

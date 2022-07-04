830cdb4f05bdfb0c4250a3623246dab37953b550
soykan <soykanerturk@gmail.com>
2022-07-04 18:16:55 +0300

    Entity class: Task completed

--- src/main/java/entity/Task.java
+++ src/main/java/entity/Task.java
@@ -1,18 +1,97 @@
 package entity;
 
-import javax.persistence.Entity;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.userdetails.User;
 
 @Entity
 public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@Column(nullable = false)
 	private String name;
-	/*
-	 * Add 
-	 * 		duration, 
-	 * 		start time, 
-	 * 		end time,
-	 * 		user (foreign key)
-	 * Here
-	 */
+
+	@Column(nullable = false)
+	private int duration;
+
+	@Column(nullable = false)
+	private LocalDate startDate;
+
+	@Column(nullable = false)
+	private LocalTime startTime;
+
+	private LocalTime endTime;
+
+	@Column(nullable = false)
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+
 }
